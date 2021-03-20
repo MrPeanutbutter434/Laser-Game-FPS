2f51df943d96f80bfd036ee0d65a43835beeb6ae
Peanutbutter <prionslop@gmail.com>
2021-03-20 19:27:47 +0530

    Added motion script to player

--- Scenes/Scripts/Player.gd
+++ Scenes/Scripts/Player.gd
@@ -2,15 +2,27 @@ extends KinematicBody
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var motion = Vector3()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	move()
+
+
+func move():
+	if Input.is_action_pressed("move_left"):
+		motion.z = -1
+	elif Input.is_action_pressed("move_right"):
+		motion.z = 1
+	else:
+		motion.z = 0
+		
+	if Input.is_action_pressed("move_forward"):
+		motion.x = 1
+	elif Input.is_action_pressed("move_backwards"):
+		motion.x = -1
+	else:
+		motion.x = 0
+		
+	move_and_slide(motion.normalized(), Vector3.UP)

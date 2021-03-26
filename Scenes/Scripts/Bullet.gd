4d60c4aca14fe06b3fc02649aa4a564344091c9f
Mr Peanutbutter <79090958+MrPeanutbutter434@users.noreply.github.com>
2021-03-26 10:10:35 +0530

    Add script to fire bullets (#3)
    * Added bullet and script
    
    * Test firing bullet
    
    - Added dummy test cube
    
    * Spawn bullet when space bar is pressed
    
    * Fire bullet when spacebar is pressed

---
+++ Scenes/Scripts/Bullet.gd
@@ -0,0 +1,25 @@
+extends Area
+
+signal exploded
+
+export var muzzle_velocity = 25
+export var g = Vector3.DOWN*20
+
+
+var velocity = Vector3.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	velocity += delta*g
+	look_at(transform.origin + velocity.normalized(), Vector3.UP)
+	transform.origin += velocity*delta
+
+
+func _on_Bullet_body_entered(body):
+	print(body)
+	emit_signal("exploded", transform.origin)
+

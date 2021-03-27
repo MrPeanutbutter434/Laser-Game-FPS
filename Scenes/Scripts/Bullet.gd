b86e865c7dc428ef4e38114ea91a254820a93e65
Mr Peanutbutter <79090958+MrPeanutbutter434@users.noreply.github.com>
2021-03-27 18:22:39 +0530

    Fix bullet path (#5)
    * Tweak bullet vars
    
    * Fixed firing of projectiles
    
    * Free bullet on collision

--- Scenes/Scripts/Bullet.gd
+++ Scenes/Scripts/Bullet.gd
@@ -3,23 +3,19 @@ extends Area
 signal exploded
 
 export var muzzle_velocity = 25
-export var g = Vector3.DOWN*20
+export var g = Vector3.DOWN*10
 
 
-var velocity = Vector3.ZERO
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export var velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
 	velocity += delta*g
-	look_at(transform.origin + velocity.normalized(), Vector3.UP)
+	look_at(transform.origin + velocity.normalized(), Vector3.FORWARD)
 	transform.origin += velocity*delta
 
 
 func _on_Bullet_body_entered(body):
-	print(body)
 	emit_signal("exploded", transform.origin)
+	queue_free()
 

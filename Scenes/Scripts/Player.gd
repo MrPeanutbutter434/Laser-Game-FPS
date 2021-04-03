68bb9a3cabf18a6f2ba8704ed6d13513fefb4be2
Peanutbutter <prionslop@gmail.com>
2021-04-03 11:43:06 +0530

    Added type safety and normalized motion

--- Scenes/Scripts/Player.gd
+++ Scenes/Scripts/Player.gd
@@ -3,18 +3,18 @@ extends KinematicBody
 
 onready var Bullet = preload("res://Scenes/Players/Bullet.tscn")
 
-const JUMP_SPEED = 18
-const GRAVITY = -24
+const JUMP_SPEED:int = 18
+const GRAVITY:int = -24
 
 var motion = Vector3()
-var MOUSE_SENSITIVITY = 0.05
+var MOUSE_SENSITIVITY: float = 0.05
 var rotation_helper
 
 func _ready():
 	rotation_helper = $RotationHelper
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _process(delta):
+func _process(delta: float)-> void:
 	move(delta)
 	shoot()
 	toggle_mouse_mode()
@@ -31,21 +31,21 @@ func toggle_mouse_mode():
 
 func move(delta):
 	if Input.is_action_pressed("move_left"):
-		motion.x = -1
+		motion.x += -1
 	elif Input.is_action_pressed("move_right"):
-		motion.x = 1
+		motion.x += 1
 	else:
 		motion.x = 0
 		
 	if Input.is_action_pressed("move_forward"):
-		motion.z = -1
+		motion.z += -1
 	elif Input.is_action_pressed("move_backwards"):
-		motion.z = 1
+		motion.z += 1
 	else:
 		motion.z = 0
 		
 	motion.y += delta*GRAVITY	
-	move_and_slide(motion, Vector3.UP)
+	move_and_slide(motion.normalized(), Vector3.UP)
 
 
 func jump():

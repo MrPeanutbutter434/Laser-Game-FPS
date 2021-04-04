23b3c4bdf5a822b7f0c6abe4158c99ce5ab2225f
Peanutbutter <prionslop@gmail.com>
2021-04-04 09:40:23 +0530

    Reduced weird sideways rotation

--- Scenes/Scripts/Player.gd
+++ Scenes/Scripts/Player.gd
@@ -9,9 +9,12 @@ const GRAVITY:int = -24
 var motion = Vector3()
 var MOUSE_SENSITIVITY: float = 0.05
 var rotation_helper
+var dir: Vector3
+var camera
 
 func _ready():
 	rotation_helper = $RotationHelper
+	camera = $Camera
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta: float)->void:
@@ -19,7 +22,6 @@ func _process(delta: float)->void:
 	shoot()
 	toggle_mouse_mode()
 	jump()
-
 
 func toggle_mouse_mode():
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -46,6 +48,13 @@ func move(delta:float)->void:
 		
 	motion.y += delta*GRAVITY	
 	move_and_slide(motion.normalized(), Vector3.UP)
+	
+	dir = Vector3()
+	
+	var cam_xform = camera.get_global_transform()
+	
+	dir += -cam_xform.basis.z * motion.z
+	dir += cam_xform.basis.x * motion.x
 
 
 func jump():
@@ -66,3 +75,7 @@ func _input(event: InputEvent):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY))
 		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
+		
+		var camera_rot = rotation_helper.rotation_degrees
+		camera_rot.x = clamp(camera_rot.x, -70, 70)
+		rotation_helper.rotation_degrees = camera_rot

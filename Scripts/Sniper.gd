7674eff637aab30d3afdb8f524abb2291d62837a
Peanutbutter <prionslop@gmail.com>
2021-05-15 11:55:55 +0530

    Add static sniper with raycast

---
+++ Scripts/Sniper.gd
@@ -0,0 +1,16 @@
+extends MeshInstance
+
+const DAMAGE = 6
+
+
+func fire():
+	var ray = $RayCast as RayCast
+	
+	ray.force_raycast_update()
+	
+	if ray.is_colliding():
+		var body = ray.get_collider()
+		
+		if body.has_method("bullet_hit"):
+			# TODO call bullet_hit method in bots
+			pass
